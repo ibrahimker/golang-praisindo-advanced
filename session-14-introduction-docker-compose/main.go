724737ab1d9a89c2e6369d463019c47b3d8e5680
Ibrahim Nurandita Isbandiputra <ibrahim.nurandita@wartek.belajar.id>
2024-07-30 17:29:41 +0700

    add connection to db in docker

--- session-14-introduction-docker-compose/main.go
+++ session-14-introduction-docker-compose/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log"
+	"time"
+)
 
 func main() {
-	fmt.Println("hello world run from docker compose")
+	time.Sleep(3 * time.Second)
+	dsn := "postgresql://postgres:password@postgres-db:5434/postgres"
+	gormDB, err := gorm.Open(postgres.Open(dsn), &gorm.Config{SkipDefaultTransaction: true})
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Println("hello world run from docker compose")
+	log.Println("hello world run from docker compose 2")
+	log.Println(gormDB)
+	sqlDB, err := gormDB.DB()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	if err := sqlDB.Ping(); err != nil {
+		log.Fatalln(err)
+	}
+	log.Println("Successfully ping db")
 }
